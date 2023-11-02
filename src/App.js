b4d4bfd7203f48b2a91175145b95b519eda32eea
Luan_ <88367567+Luan16p@users.noreply.github.com>
2023-11-02 11:35:17 -0400

    chore: update index files for achieve greater performance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from './components/Header';
 
 import './style/global.css';
+import './style/responsive.css'
 
 import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Header/>
+
     </BrowserRouter>
   );
 }
