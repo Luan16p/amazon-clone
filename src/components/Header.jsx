8e375e1f8116faecf3787d9a1d5777280b136d27
Luan_ <88367567+Luan16p@users.noreply.github.com>
2023-11-02 11:32:08 -0400

    chore: finish Header component and add file for responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import '../style/header.css'
 
+import { Link } from 'react-router-dom';
+
 import { useState } from 'react';
 
 import { IoLocationOutline } from 'react-icons/io5';
 import { AiFillCaretDown } from 'react-icons/ai';
 import { FaSearch } from 'react-icons/fa'
 import { BsMinecartLoaded } from 'react-icons/bs'
+import { BiMenu } from 'react-icons/bi'
 
-export default function Header() {
+export default function Header({ contextCarrinho }) {
     const [border, setBorder] = useState(false);
 
 
@@ -27,7 +30,7 @@ export default function Header() {
                         <p className='info-entrega'>
                             A entrega será feita em Bela vista 01319
                         </p>
-                        <p className='atualizar'>
+                        <p className='atualizar p-bold link'>
                             Atualizar Local
                         </p>
                     </div>
@@ -42,19 +45,36 @@ export default function Header() {
                 <div className="navlinks-amazon">
                     <div className="account">
                         <p>Olá, faça seu login</p>
-                        <p className='login p-bold'>Contas e Listas <AiFillCaretDown /></p>
+                        <p className='login p-bold link'>Contas e Listas <AiFillCaretDown /></p>
                     </div>
 
                     <div className="devolucao">
                         <p>Devoluções</p>
-                        <p className='p-bold'>e Pedidos</p>
+                        <p className='p-bold link'>e Pedidos</p>
                     </div>
 
-                    <div className="carrinho">
-                        <h2><BsMinecartLoaded /></h2>
-                        <p className='p-bold'>Carrinho</p>
-                    </div>
                 </div>
+                <div className="carrinho" id='carrinho'>
+                    <h2><BsMinecartLoaded /> <span>{contextCarrinho}</span></h2>
+                    <p className='p-bold link'>Carrinho</p>
+                </div>
+            </nav>
+
+            <nav className="nav-down">
+                <li id='menu-responsivo'><Link to={"/"}><BiMenu className='menu-mobile'/> Mais</Link></li>
+                <ul className='nav-down-list'>
+                    <li><Link to={"/"}><BiMenu className='menu-mobile'/> Todos</Link></li>
+                    <li><Link to={"/"}>Venda na Amazon</Link></li>
+                    <li><Link to={"/"}>Mais Vendidos</Link></li>
+                    <li><Link to={"/"}>Ofertas do Dia</Link></li>
+                    <li><Link to={"/"}>Prime <AiFillCaretDown className='icon-down-gray'/></Link></li>
+                    <li><Link to={"/"}>Novidades da Amazon</Link></li>
+                    <li><Link to={"/"}>Livros</Link></li>
+                    <li><Link to={"/"}>Música</Link></li>
+                    <li><Link to={"/"}>Ideias de Presente</Link></li>
+                </ul>
+
+                <h2 className='warn-amazon-nav'>Chegou o Amazon Quase Novo</h2>
             </nav>
         </header>
     );
